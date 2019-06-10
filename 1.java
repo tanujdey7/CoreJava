fa74e1dec25b3dfa4fd5174903ec65c9ed1ab836
Tanuj Dey <tanujdey7@gmail.com>
2019-06-10 13:09:57 +0530

    Delete 1.java

--- 1.java
+++
@@ -1,7 +0,0 @@
-class demo {
-	public static void main(String[] args) {
-		System.out.println("Welcome to JAVA World");
-		System.out.printf("Welcome to JAVA World");
-		System.out.print("Welcome to JAVA World");
-	}
-}
